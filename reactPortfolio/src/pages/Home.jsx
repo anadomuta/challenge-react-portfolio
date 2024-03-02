14751e757dddebb1da35be433520fbb53827c361
anadomuta <ana.domuta@hotmail.de>
2024-03-02 16:11:51 +0000

    adds navbar placeholder and folder structure

---
+++ reactPortfolio/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+    <p>
+      Hi! Welcome to my portfolio page! My name is Ana and I'm currently
+      learning Web Development. Learn more about my journey in the About Me
+      section.
+    </p>
+  </div>
+);
+
+export default Home;
